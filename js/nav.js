d48018dfbde4a19ee2b36a4eb11dd0b7f4546552
Carlos <carloseckert1@gmail.com>
2022-02-02 00:27:47 -0500

    first commit

---
+++ js/nav.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+// function navSubmitClick() {
+//   $submitPage.show();
+//   hide everything else
+
+// }
+
+// function navSubmitPost() {
+//   $submitPage.hide();
+    // add post to storylist
+//   $navSubmit.show();
+// }
+
+// $navSubmit.on("click", navSubmitClick);
+// $submitNewPost.on("click", navSubmitPost);
